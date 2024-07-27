630007c9e0873e85e16e4468992ad1c4940f6ed7
Aryo Nazaradeh <aryo.nazaradeh@gmail.com>
2024-07-27 00:31:48 -0700

    Add variables for solver

--- solver.cpp
+++ solver.cpp
@@ -1,8 +1,24 @@
 #include "grid.hpp"
 
 void solvePuzzle() {
-std::array<int, 10> markedOffInRow;
-std::array<int, 10> markedOffInColumn;
-std::array<int, 10> markedOffInRegion;
-for (auto& row : cellStates) {row.fill(EMPTY);}
+
+std::array<int, 10> markedOffInRow = {0};
+std::array<int, 10> markedOffInColumn = {0};
+std::array<int, 10> markedOffInRegion = {0};
+std::array<int, 30> segmentSizes = {0};
+std::array<int, 30> segmentOrder;
+
+for (int row = 0; row < 10; ++row) {
+	for (int col = 0; col < 10; ++col) {
+		if (cellStates[row][col] == MARKEDOFF) {
+			++markedOffInRow[row];
+			++markedOffInColumn[col];
+			++markedOffInRegion[regions[row][col]];
+			--segmentSizes[row];
+			--segmentSizes[10 + col];
+		}
+		else ++segmentSizes[20 + regions[row][col]];
+	}
+}
+
 }

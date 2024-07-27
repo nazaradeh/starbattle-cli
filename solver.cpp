64736b5ce068ecb588d0cc943ff10f59cb7fbd1e
Aryo Nazaradeh <aryo.nazaradeh@gmail.com>
2024-07-26 21:14:31 -0700

    Create solver.cpp

---
+++ solver.cpp
@@ -0,0 +1,8 @@
+#include "grid.hpp"
+
+void solvePuzzle(const std::array<std::array<int, 10>, 10>& regions) {
+std::array<int, 10> markedOffInRow;
+std::array<int, 10> markedOffInColumn;
+std::array<int, 10> markedOffInRegion;
+for (auto& row : cellStates) {row.fill(EMPTY);}
+}

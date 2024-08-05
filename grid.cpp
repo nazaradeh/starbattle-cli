854937fc2d7c9303bf60ff9d765c46603e5280c2
Aryo Nazaradeh <aryo.nazaradeh@gmail.com>
2024-08-04 20:47:18 -0700

    Improve readability of grid.cpp

--- grid.cpp
+++ grid.cpp
@@ -3,16 +3,16 @@
 #include <algorithm>
 #include <ranges>
 
-std::string cellElement(const int& row, const int& col) {
+std::string cellElement(int row, int col) {
 	switch (cellStates[row][col]) {
 		case EMPTY:
 			return "   ";
 		case MARKEDOFF:
 			return " \x1b[90m·\x1b[39m "; // Grey
 		case STAR: 
-			if (std::ranges::all_of(starsInRow, [](const int& x) {return x == 2;}) &&
-				std::ranges::all_of(starsInColumn, [](const int& x) {return x == 2;}) &&
-				std::ranges::all_of(starsInRegion, [](const int& x) {return x == 2;})) {
+			if (std::ranges::all_of(starsInRow, [](int n) {return n == 2;}) &&
+				std::ranges::all_of(starsInColumn, [](int n) {return n == 2;}) &&
+				std::ranges::all_of(starsInRegion, [](int n) {return n == 2;})) {
 				return " \x1b[32m★\x1b[39m "; // Green
 			}
 			if (starsInRow[row] >= 3 || starsInColumn[col] >= 3 || starsInRegion[regions[row][col]] >= 3)
@@ -23,27 +23,44 @@ std::string cellElement(const int& row, const int& col) {
 	}
 }
 
+void cellUnderside(std::string& grid, int row) { 
+	for (const int col : std::views::iota(0,9)) {
+		// Top two cells in different regions (█), all cells in the same region (┼), otherwise (▄)
+		if (regions[row][col] != regions[row][col+1]) {
+			if (regions[row][col] == regions[row+1][col]) grid.append("───█");
+			else grid.append("▄▄▄█");
+		}
+		else if (regions[row+1][col] == regions[row+1][col+1] && regions[row][col] == regions[row+1][col]) {
+			grid.append("───┼");
+		}
+		else {
+			if (regions[row][col] == regions[row+1][col]) grid.append("───▄");
+			else grid.append("▄▄▄▄");
+		}
+	}
+	// Final cell
+	if (regions[row][9] == regions[row+1][9]) grid.append("───█\n█");
+	else grid.append("▄▄▄█\n█");
+}
+
+// The main function
 std::string buildGrid() {
 	std::string grid("\x1b[H\x1b[39m▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄\n█"); // Move to top of screen and print ceiling
-	for (const int& row : std::views::iota(0,10)) {
+	for (int row : std::views::iota(0,10)) {
 		// Gameplay rows
-		for (const int& col : std::views::iota(0,10)) {
+		for (int col : std::views::iota(0,10)) {
 			grid.append(cellElement(row, col)); // Inside the cell itself
-			if (col < 9) grid.append(regions[row][col] == regions[row][col+1] ? "│" : "█"); // Divider between two horizontally-adjacent cells
-			else grid.append("█\n█"); // Right wall
+			if (col == 9) grid.append("█\n█"); // Right wall
+			else if (regions[row][col] == regions[row][col+1]) grid.append("│"); // Divider within the same region
+			else grid.append("█"); // Divider between two regions
 		}
 		// Border rows
-		if (row < 9) {
-			for (const int& col : std::views::iota(0,9)) {
- 				// Top two cells in different regions (█), all cells in the same region (┼), otherwise (▄)
-				if (regions[row][col] != regions[row][col+1]) grid.append(regions[row][col] == regions[row+1][col] ? "───█" : "▄▄▄█");
-				else if (regions[row+1][col] == regions[row+1][col+1] && regions[row][col] == regions[row+1][col]) grid.append("───┼");
-				else {grid.append(regions[row][col] == regions[row+1][col] ? "───▄" : "▄▄▄▄");}
-			}
-			// Final cell
-			grid.append(regions[row][9] == regions[row+1][9] ? "───█\n█" : "▄▄▄█\n█");
-		}
-		else for (const int& col : std::views::iota(0,10)) grid.append(col < 9 && regions[9][col] == regions[9][col+1] ? "▄▄▄▄" : "▄▄▄█");
+		if (row < 9) cellUnderside(grid, row); // Border row between two vertically-adjacent gameplay rows
+	}
+	// Floor row
+	for (const int col : std::views::iota(0,10)) {
+		if (col < 9 && regions[9][col] == regions[9][col+1]) grid.append("▄▄▄▄");
+		else grid.append("▄▄▄█");
 	}
 	return grid;
 }

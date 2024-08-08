4f2330534d84a035839359c1e570b2389486ce1b
Aryo Nazaradeh <aryo.nazaradeh@gmail.com>
2024-08-07 23:44:44 -0700

    Add Board class

--- globals.hpp
+++ globals.hpp
@@ -10,4 +10,20 @@ extern std::array<int, 10> starsInColumn;
 extern std::array<int, 10> starsInRegion;
 const std::array<std::pair<int, int>, 8> NEIGHBOURS = {{{-1, -1}, {-1, 0}, {-1, 1}, {0, -1}, {0, 1}, {1, -1}, {1, 0}, {1, 1}}};
 
+class Board {
+public:
+	Board();
+	std::array<std::array<int, 10>, 10> regions;
+	void toggleCellState();
+	void printGrid() const;
+
+private:
+	std::array<std::array<CellState, 10>, 10> cellStates;
+	std::array<int, 10> starsInRow;
+	std::array<int, 10> starsInColumn;
+	std::array<int, 10> starsInRegion;
+};
+
+extern Board board;
+
 #endif

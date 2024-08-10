43c92bc6db7b281e721204eb821582fda02053d1
Aryo Nazaradeh <aryo.nazaradeh@gmail.com>
2024-08-09 21:29:36 -0700

    Start implementing Board class

--- globals.hpp
+++ globals.hpp
@@ -1,27 +1,27 @@
 #ifndef GLOBALS_HPP
 #define GLOBALS_HPP
 #include <array>
-
-extern std::array<std::array<int, 10>, 10> regions;
+/*
+extern std::array<std::array<int8_t, 10>, 10> regions;
 enum CellState {EMPTY, MARKEDOFF, STAR};
 extern std::array<std::array<CellState, 10>, 10> cellStates;
-extern std::array<int, 10> starsInRow;
-extern std::array<int, 10> starsInColumn;
-extern std::array<int, 10> starsInRegion;
-const std::array<std::pair<int, int>, 8> NEIGHBOURS = {{{-1, -1}, {-1, 0}, {-1, 1}, {0, -1}, {0, 1}, {1, -1}, {1, 0}, {1, 1}}};
+extern std::array<int8_t, 10> starsInRow;
+extern std::array<int8_t, 10> starsInColumn;
+extern std::array<int8_t, 10> starsInRegion;
+*/
+const std::array<std::pair<int8_t, int8_t>, 8> NEIGHBOURS = {{{-1, -1}, {-1, 0}, {-1, 1}, {0, -1}, {0, 1}, {1, -1}, {1, 0}, {1, 1}}};
 
 class Board {
 public:
-	Board();
-	std::array<std::array<int, 10>, 10> regions;
+	//Board();
+	std::array<std::array<int8_t, 10>, 10> regions;
 	void toggleCellState();
 	void printGrid() const;
-
-private:
+	enum CellState {EMPTY, MARKEDOFF, STAR};
 	std::array<std::array<CellState, 10>, 10> cellStates;
-	std::array<int, 10> starsInRow;
-	std::array<int, 10> starsInColumn;
-	std::array<int, 10> starsInRegion;
+	std::array<int8_t, 10> starsInRow;
+	std::array<int8_t, 10> starsInColumn;
+	std::array<int8_t, 10> starsInRegion;
 };
 
 extern Board board;
--- solver.cpp
+++ solver.cpp
@@ -29,23 +29,23 @@ std::vector<std::pair<int, int>> getPerimeterCells(const int& region, const std:
 		for (const auto& offset : ORTHONEIGHBOURS) {
 			int y = cell.first + offset.first;
 			int x = cell.second + offset.second;
-			if (y >= 0 && y < 10 && x >= 0 && x < 10 && regions[y][x] != region) perimeterSet.emplace(y, x);
+			if (y >= 0 && y < 10 && x >= 0 && x < 10 && board.regions[y][x] != region) perimeterSet.emplace(y, x);
 		}
 	}
 	return ranges::to<std::vector>(perimeterSet);
 }
 
 void placeStar(const int& region, const std::pair<int, int>& coord) {
-	cellStates[coord.first][coord.second] = STAR;
+	board.cellStates[coord.first][coord.second] = Board::STAR;
 	trackedCells.push_back(coord);
-	++starsInRegion[regions[coord.first][coord.second]];
-	--segmentSizes[20 + regions[coord.first][coord.second]];
+	++board.starsInRegion[board.regions[coord.first][coord.second]];
+	--segmentSizes[20 + board.regions[coord.first][coord.second]];
 	for (const auto& [yOff, xOff] : NEIGHBOURS) {
 		if (coord.first + yOff > -1 && coord.first + yOff < 10
 			&& coord.second + xOff > -1 && coord.second + xOff < 10
-			&& regions[coord.first + yOff][coord.second + xOff] == region
-			&& cellStates[coord.first + yOff][coord.second + xOff] == EMPTY) {
-			cellStates[coord.first + yOff][coord.second + xOff] = MARKEDOFF;
+			&& board.regions[coord.first + yOff][coord.second + xOff] == region
+			&& board.cellStates[coord.first + yOff][coord.second + xOff] == Board::EMPTY) {
+			board.cellStates[coord.first + yOff][coord.second + xOff] = Board::MARKEDOFF;
 			trackedCells.push_back({coord.first + yOff, coord.second + xOff});
 			--segmentSizes[20 + region];
 		}
@@ -54,9 +54,9 @@ void placeStar(const int& region, const std::pair<int, int>& coord) {
 
 void backtrack(const auto& cells, const std::optional<int>& trackedCellsSizeOld = std::nullopt) {
 	for (const auto& [y, x] : cells | views::drop(trackedCellsSizeOld.value_or(0))) {
-		if (cellStates[y][x] == STAR) --starsInRegion[regions[y][x]];
-		cellStates[y][x] = EMPTY;
-		++segmentSizes[20 + regions[y][x]];
+		if (board.cellStates[y][x] == Board::STAR) --board.starsInRegion[board.regions[y][x]];
+		board.cellStates[y][x] = Board::EMPTY;
+		++segmentSizes[20 + board.regions[y][x]];
 	}
 	trackedCells.resize(trackedCellsSizeOld.value_or(0));
 }
@@ -65,47 +65,41 @@ void markOffCellsInLine(const int region, const auto& regionEmptyCells) {
 	const int& regionRow = regionEmptyCells.front().first;
 	const int& regionCol = regionEmptyCells.front().second;
 	if (ranges::all_of(regionEmptyCells | views::drop(1), [&regionRow](const auto& coord) {return coord.first == regionRow;})) {
-		for (const int& col : views::iota(0, 10) | views::filter([&](const int& col) {return regions[regionRow][col] != region;})) {
-			cellStates[regionRow][col] = MARKEDOFF;
-		}
-	}
-	else if (ranges::all_of(regionEmptyCells | views::drop(1), [&regionCol](const auto& coord) {return coord.second == regionCol;})) {
-		for (const int& row : views::iota(0, 10) | views::filter([&](const int& row) {return regions[row][regionCol] != region;})) {
-			cellStates[row][regionCol] = MARKEDOFF;
-		}
+		for (const int& col : views::iota(0, 10) | views::filter([&](const int& col) {return board.regions[regionRow][col] != region;}))
+			board.cellStates[regionRow][col] = Board::MARKEDOFF;
+	} else if (ranges::all_of(regionEmptyCells | views::drop(1), [&regionCol](const auto& coord) {return coord.second == regionCol;})) {
+		for (const int& row : views::iota(0, 10) | views::filter([&](const int& row) {return board.regions[row][regionCol] != region;}))
+			board.cellStates[row][regionCol] = Board::MARKEDOFF;
 	}
 }
 
 void perimeterCellCheck(const auto& perimeterCell, const int region, const auto& regionEmptyCells) {
-	placeStar(region, {perimeterCell}); // Place star and surround with MARKEDOFF
+	placeStar(region, {perimeterCell}); // Place star and surround with Board::MARKEDOFF
 	// The outside star already causes the region to have too few available cells
-	if (segmentSizes[20 + region] < 2 - starsInRegion[region]) {
+	if (segmentSizes[20 + region] < 2 - board.starsInRegion[region]) {
 		backtrack(trackedCells);
-		cellStates[perimeterCell.first][perimeterCell.second] = MARKEDOFF;
-		--segmentSizes[20 + regions[perimeterCell.first][perimeterCell.second]];
+		board.cellStates[perimeterCell.first][perimeterCell.second] = Board::MARKEDOFF;
+		--segmentSizes[20 + board.regions[perimeterCell.first][perimeterCell.second]];
 	}
 	// The outside star still allows for the region's remaining two stars to be placed
-	else if (starsInRegion[region] == 0) {
+	else if (board.starsInRegion[region] == 0) {
 		bool skip = false;
 		for (auto& cellCoord2 : regionEmptyCells
-			| views::filter([&](const auto& coord) {return cellStates[coord.first][coord.second] == EMPTY;})) { // Go through each cell in region 0 again	
+			| views::filter([&](const auto& coord) {return board.cellStates[coord.first][coord.second] == Board::EMPTY;})) { // Go through each cell in region 0 again	
 			int trackedCellsSizeOld = trackedCells.size();
 			placeStar(region, cellCoord2); // Try placing a star to see if another star can fit inside
 			if (segmentSizes[20 + region] > 0) { // Another star can fit inside, meaning we cannot rule out the perimeter cell
 				backtrack(trackedCells);
 				skip = true;
 				break;
-			}
-			else backtrack(trackedCells, trackedCellsSizeOld); // No other star can fit inside. Keep checking other inner cells.
+			} else backtrack(trackedCells, trackedCellsSizeOld); // No other star can fit inside. Keep checking other inner cells.
 		}
 		if (!skip) { // There is no way to fit both stars in the region, given this perimeter star placement
 			backtrack(trackedCells);
-			cellStates[perimeterCell.first][perimeterCell.second] = MARKEDOFF;
-			--segmentSizes[20 + regions[perimeterCell.first][perimeterCell.second]];
+			board.cellStates[perimeterCell.first][perimeterCell.second] = Board::MARKEDOFF;
+			--segmentSizes[20 + board.regions[perimeterCell.first][perimeterCell.second]];
 		}
-	}
-	// The outside star still allows for the regions one remaining star to be placed
-	else if (starsInRegion[region] == 1) backtrack(trackedCells);
+	} else if (board.starsInRegion[region] == 1) backtrack(trackedCells); // The outside star still allows for the board.regions one remaining star to be placed
 }
 
 // The main function
@@ -114,16 +108,15 @@ void solvePuzzle() {
 	// Go through the grid to gather info on each row, column, and region
 	for (const int& row : views::iota(0, 10)) {
 		for (const int& col : views::iota(0, 10)) {
-			regionCells[regions[row][col]].emplace_back(row, col);
-			if (cellStates[row][col] == MARKEDOFF) {
+			regionCells[board.regions[row][col]].emplace_back(row, col);
+			if (board.cellStates[row][col] == Board::MARKEDOFF) {
 				++markedOffInRow[row];
 				++markedOffInColumn[col];
-				++markedOffInRegion[regions[row][col]];
-			}
-			else {
+				++markedOffInRegion[board.regions[row][col]];
+			} else {
 				++segmentSizes[row];
 				++segmentSizes[10 + col];
-				++segmentSizes[20 + regions[row][col]];
+				++segmentSizes[20 + board.regions[row][col]];
 			}
 		}
 	}
@@ -132,13 +125,13 @@ void solvePuzzle() {
 	for (int row : views::iota(0, 10)) {
 		const auto& rowEmptyCells = ranges::to<std::vector>(views::iota(0, 10)
 			| views::transform([row](int col) {return std::pair{row, col};})
-			| views::filter([&](const auto& coord) {return cellStates[coord.first][coord.second] != MARKEDOFF;}));
-		int region = regions[rowEmptyCells[0].first][rowEmptyCells[1].second];
+			| views::filter([&](const auto& coord) {return board.cellStates[coord.first][coord.second] != Board::MARKEDOFF;}));
+		int region = board.regions[rowEmptyCells[0].first][rowEmptyCells[1].second];
 
 		// If the only cells in a row not marked off are all in the same region, mark off the parts of said region that is not in this row
-		if (ranges::all_of(rowEmptyCells | views::drop(1), [&region](const auto& coord) {return regions[coord.first][coord.second] == region;})) {
+		if (ranges::all_of(rowEmptyCells | views::drop(1), [&region](const auto& coord) {return board.regions[coord.first][coord.second] == region;})) {
 			for (const auto& coord : regionCells[region] | views::filter([&](auto coord) {return coord.first != row;})) {
-				cellStates[coord.first][coord.second] = MARKEDOFF;
+				board.cellStates[coord.first][coord.second] = Board::MARKEDOFF;
 			}
 		} 
 		// I might want to do more things with my row of empty cells, here.
@@ -149,12 +142,12 @@ void solvePuzzle() {
 
 		// If this region's cells form a straight line, mark off the rest of the cells on that row or column
 		const auto& regionEmptyCells = ranges::to<std::vector>(regionCells[region]
-			| views::filter([&](const auto& coord) {return cellStates[coord.first][coord.second] != MARKEDOFF;}));
+			| views::filter([&](const auto& coord) {return board.cellStates[coord.first][coord.second] != Board::MARKEDOFF;}));
 
 		markOffCellsInLine(region, regionEmptyCells);
 		
 		for (const std::pair<int, int>& perimeterCell : getPerimeterCells(region, regionEmptyCells)
-			| views::filter([&](const auto& cell) {return cellStates[cell.first][cell.second] == EMPTY;})) { // Go through the orthogonal perimeter of region
+			| views::filter([&](const auto& cell) {return board.cellStates[cell.first][cell.second] == Board::EMPTY;})) { // Go through the orthogonal perimeter of region
 			perimeterCellCheck(perimeterCell, region, regionEmptyCells);	
 		}
 	}

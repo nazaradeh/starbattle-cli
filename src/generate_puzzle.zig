d6a726ac32d194a0fd23d995432b0314a7fd7269
Aryo Nazaradeh <aryo.nazaradeh@gmail.com>
2025-04-29 23:09:50 -0700

    Add loop to determine sizes of each region

--- src/generate_puzzle.zig
+++ src/generate_puzzle.zig
@@ -1,7 +1,9 @@
+const heap = @import("std").heap;
 const globals = @import("globals.zig");
+const Point = globals.Point;
 const CellState = globals.CellState;
 
-pub fn generatePuzzle() void {
+pub fn generatePuzzle() !void {
     globals.board.regions = .{
         .{ 0, 0, 0, 1, 2, 2, 2, 2, 2, 2 },
         .{ 0, 3, 1, 1, 2, 2, 2, 2, 2, 2 },
@@ -15,7 +17,13 @@ pub fn generatePuzzle() void {
         .{ 6, 6, 9, 9, 9, 9, 9, 5, 5, 5 },
     };
     globals.board.cellStates = [_][10]CellState{[_]CellState{CellState.EMPTY} ** 10} ** 10;
-    for (globals.board.regions, 0..10) |regionsRow, row| {
-        for (regionsRow, 0..10) |region, col| globals.board.regionCells[region].pushback = .{ row, col };
+    var regionSizes: [10]usize = [_]usize{0} ** 10;
+    for (0..10) |row| {
+        for (0..10) |col| regionSizes[globals.board.regions[row][col]] += 1;
     }
+    //var gpa = heap.GeneralPurposeAllocator(.{}){};
+    //defer _ = gpa.deinit();
+    //for (0..10) |region| {
+    //    globals.board.regionCells[region] = try gpa.allocator().alloc(Point, regionSizes[region]);
+    //}
 }
--- src/main.zig
+++ src/main.zig
@@ -5,7 +5,7 @@ const globals = @import("globals.zig");
 const generate_puzzle = @import("generate_puzzle.zig");
 const grid = @import("grid.zig");
 
-pub fn main() void {
-    generate_puzzle.generatePuzzle();
+pub fn main() !void {
+    try generate_puzzle.generatePuzzle();
     print("{}\n", .{globals.board.regions[2][2]});
 }
